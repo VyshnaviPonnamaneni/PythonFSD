a7fc6da73bbcd6aeb4321b1e7b12a521584713c8
VyshnaviPonnamaneni <104306288+VyshnaviPonnamaneni@users.noreply.github.com>
2024-10-30 01:01:07 +0000

    added grocery calculation and saving caluculation programs

---
+++ d_grocery_store.py
@@ -0,0 +1,8 @@
+cost_of_rice = 10
+cost_of_wheat = 34
+quanity_of_rice = int(input("enter the quantity of rice?"))
+quanity_of_wheat = int(input("enter the quantity of wheat?"))
+total_cost_of_rice = cost_of_rice * quanity_of_rice
+total_cost_of_wheat = cost_of_wheat * quanity_of_wheat
+print(total_cost_of_rice)
+print(total_cost_of_wheat)
